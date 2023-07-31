e961ce0886da9025ccf100b0fe74954f4a46173b
Juan José Gómez A <jjgomeza22@gmail.com>
2023-07-30 20:23:38 -0500

    permit confirm exit to the page, its util for avoid terminate incompleted form

--- src/app/website/pages/register/register.component.ts
+++ src/app/website/pages/register/register.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { onExit } from 'src/app/guards/exit.guard';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements onExit {
 
+  onExit () {
+    const rta = confirm('logica desde component, si quieres salir?')
+    return rta;
+  }
 }
--- src/app/website/website-routing.module.ts
+++ src/app/website/website-routing.module.ts
@@ -11,6 +11,7 @@ import { RegisterComponent } from './pages/register/register.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { AuthGuard } from '../guards/auth.guard';
+import { ExitGuard } from '../guards/exit.guard';
 
 const routes: Routes = [
   {
@@ -56,6 +57,7 @@ const routes: Routes = [
       },
       {
         path: 'register',
+        canDeactivate: [ExitGuard],
         component: RegisterComponent
       },
     ]
